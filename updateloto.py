5ed4c9d0bb611ae01e91449af7a130521ca6d902
santhkhd <santhkhd@gmail.com>
2025-09-04 15:42:30 +0530

    Update updateloto.py

--- updateloto.py
+++ updateloto.py
@@ -12,7 +12,8 @@ IST = pytz.timezone('Asia/Kolkata')
 
 # Add headers to mimic a web browser
 HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Referer': 'https://www.kllotteryresult.com/'
 }
 
 # GitHub Configuration
